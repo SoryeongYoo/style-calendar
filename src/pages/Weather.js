d1d39d42b589d0cf185801c350c3e9abb6491238
unknown <soryung11@naver.com>
2024-12-20 16:47:34 +0900

    옷 추천 기능
    날씨에 맞는 옷 추천

--- src/pages/Weather.js
+++ src/pages/Weather.js
@@ -4,6 +4,8 @@ const Weather = () => {
   const [weather, setWeather] = useState(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const [clothingSuggetion, setClothingSuggestion] = useState('')
+  const [clothing, setClothing] = useState()
 
   const API_KEY = 'd871577fb0306dcaf9c2e88ec14bfac2'
 
@@ -25,7 +27,7 @@ const Weather = () => {
         setError('Geolocation is not supported by this browser');
         setLoading(false);
       }
-    };
+    }
 
     const fetchWeather = async (latitude, longitude) => {
       try {
@@ -48,6 +50,37 @@ const Weather = () => {
       } finally {
         setLoading(false)
       }
+    }
+
+    //날씨에 맞는 옷 추천
+    const generateClothingSuggestion=(data)=>{
+      const temp =data.main.temp //현재 온도
+      const weatherCondition = data.weather[0].main.toLowerCase() //날씨 상태 문자 반환
+
+      let suggestion = ''
+      let clothSuggestion= ''
+
+      if (temp>25){
+        suggestion='오늘은 가벼운 옷 어떨까요?'
+        //---추천 옷 img 랜덤 추가---//
+      }else if(temp>=15){
+        suggestion='오늘은 가벼운 아우터를 걸치는게 좋겠어요!'
+        //---추천 옷 img 랜덤 추가---//
+      }else if(temp>=5){
+        suggestion='감기 걸리지 않게 따뜻하게 입고 가세요!'
+        //---추천 옷 img 랜덤 추가---//
+      }else{
+        suggestion='준비 단단히 하고 나가시는게 좋겠어요! 장갑, 목도리 챙기세요!'
+        //---추천 옷 img 랜덤 추가---//
+      }
+      
+      if(weatherCondition.includes('rain') || weatherCondition.includes('snow')){
+        suggestion+='장화, 우산 잊지 마세요!'
+        //---장화 등등 img 랜덤 추가---//
+      }
+
+      setClothingSuggestion(suggestion)
+      //setClothing()
     }
 
     getLocation()
