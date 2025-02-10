e07e9f0495c1ca1e6f8546e61c6cbe5f11837e74
unknown <soryung11@naver.com>
2025-02-10 22:31:39 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Calendar from './pages/Calendar';
+import CalendarPage from './pages/CalendarPage';
 import Closet from './pages/Closet';
+import Login from './pages/Login';
+import Mypage from './pages/Mypage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/CalendarPage" element={<CalendarPage />} />
         <Route path="/closet" element={<Closet />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/mypage" element={<Mypage />} />
       </Routes>
     </Router>
   );
