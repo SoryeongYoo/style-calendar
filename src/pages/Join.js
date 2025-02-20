34edace3a1ca952498fe1abc752a426ff202ca73
unknown <soryung11@naver.com>
2025-02-20 12:32:09 +0900

    password
    password 확인

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -1,18 +1,31 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
 import React, { useState } from 'react'
 import { auth } from './firebaseConfig'
 import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom'
 
 const Join = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [error, setError] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+
+  const navigate = useNavigate()
+  const auth = getAuth()
 
   const handleJoin = async (e) => {
     e.preventDefault();
+
+    if(password !== confirmPassword){
+      setError("비밀번호가 일치하지 않습니다.")
+      return
+    }
+
     try {
       await createUserWithEmailAndPassword(auth, email, password)
       alert("회원가입 성공")
+      navigate('/login')
+      
     } catch (err) {
       setError(err.message)
     }
@@ -34,9 +47,16 @@ const Join = () => {
         <input
           type='password'
           id='password'
-
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          placeholder='Enter your password'
+        />
+        <label htmlFor="password">Password 확인</label>
+        <input
+          type='password'
+          id='password'
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
           placeholder='Enter your password'
         />
         {error && <ErrorMessage>{error}</ErrorMessage>}
