1bacceea97725ab8dc72762eb5d459b06a17ca83
unknown <soryung11@naver.com>
2025-02-27 13:57:12 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,26 @@ import CalendarPage from './pages/CalendarPage';
 import Closet from './pages/Closet';
 import Login from './pages/Login';
 import Mypage from './pages/Mypage';
+import DateDetailPage from './pages/DateDetailPage';
+import Join from './pages/Join';
+import PrivateRoute from './pages/PrivateRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/CalendarPage" element={<CalendarPage />} />
+        <Route path="/calendar" element={
+          <PrivateRoute>
+            <CalendarPage />
+          </PrivateRoute>
+          } 
+        />
         <Route path="/closet" element={<Closet />} />
         <Route path="/login" element={<Login />} />
         <Route path="/mypage" element={<Mypage />} />
+        <Route path="/calendar/:date" element={<DateDetailPage />} />
+        <Route path="/join" element={<Join />} />
       </Routes>
     </Router>
   );
